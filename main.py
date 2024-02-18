077749cfe85aa20a514db7497bd8e474fb539358
paul <pgtech246@gmail.com>
2024-02-17 23:14:56 -0400

    Adjust window font

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My Todo App", layout=[[label], [input_box, add_button]])
+window = sg.Window("My Todo App", 
+                   layout=[[label], [input_box, add_button]], 
+                   font=("Helvetica", 11))
 window.read()
 window.close()
