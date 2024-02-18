741992116199042bf17ca72afc662bc2af5fe6bc
paul <pgtech246@gmail.com>
2024-02-17 23:36:20 -0400

    Add "add button" function and close window using window x without error

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import readwrite
+import readwrite as rw
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a todo")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
 window = sg.Window("My Todo App", 
                    layout=[[label], [input_box, add_button]], 
                    font=("Helvetica", 11))
-window.read()
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = rw.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            rw.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
