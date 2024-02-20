d0af5e5e3c125dfc4b3581843a7c7e4b9ec28099
paul <pgtech246@gmail.com>
2024-02-20 14:58:30 -0400

    Update add field to clear upon addition of item

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ while True:
             todos.append(new_todo)
             rw.write_todos(todos)
             window["todos"].update(values=todos)
+            window["todo"].update(value="")
 
         case "Edit":
             try:
